292d26286a27f1a496bf5fbeb6b187bd82ef1288
Ballér Pál Krisztián <deadfair666@gmail.com>
2021-08-16 10:54:01 +0200

    day view

--- src/app/fullcalendar-day-view/fullcalendar-day-view.component.ts
+++ src/app/fullcalendar-day-view/fullcalendar-day-view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TravelEventInfo } from './../interface/travelEventInfo';
-import { addDays, asCleanDays, Calendar, CalendarOptions, DayCellContent } from '@fullcalendar/angular';
+import { addDays, asCleanDays, Calendar, CalendarOptions, DayCellContent, FullCalendarComponent } from '@fullcalendar/angular';
 import { TravelPlusEventsInfo } from '../interface/travelPlusEventsInfo';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-fullcalendar-day-view',
@@ -20,7 +21,22 @@ export class FullcalendarDayViewComponent implements OnInit {
     return Array(n).fill(1);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
+  @ViewChild('calendar')
+  calendarComponent!: FullCalendarComponent;
+
+  backDay(){
+
+    const calendarApi = this.calendarComponent.getApi();
+    calendarApi.prev()
+  }
+  nextDay() {
+      const calendarApi = this.calendarComponent.getApi();
+      calendarApi.next()
+  }
 
   closeDeleteWindow(id:string|null){
     this.eventwindow=false;
@@ -42,6 +58,7 @@ export class FullcalendarDayViewComponent implements OnInit {
     this.eventwindow=true;
   }
 
+
   calendarOptions: CalendarOptions = {
     headerToolbar:{
       left:'',
@@ -50,8 +67,14 @@ export class FullcalendarDayViewComponent implements OnInit {
     },
     initialView: 'timeGridDay',
     eventDidMount: function(info) {
+      var node = document.createElement("div");
+      node.className="event-text-container"
+      node.innerHTML=`<div class="event-text">${info.event.extendedProps.eventText}</div>`    // szülő szélessége - x
+      // az event kerüljön bele
+      // klasszz adódjon hozzá
+      info.el.children[0].appendChild(node)
       console.log(info)
-    },
+    },   // pointerevent felülvizsgálata a másik view ba tágitás miatt
     editable: true,
     moreLinkClick:(info)=>{
       this.eventwindow=false;
@@ -79,7 +102,8 @@ export class FullcalendarDayViewComponent implements OnInit {
       { id:"04",title: 'Evenasdt', date: '2021-08-01', backgroundColor:'green' }, // eventek
       { id:"05",title: 'Eveffdnt', date: '2021-08-01' }, // eventek
       { id:"06",title: 'Event', date: '2021-08-01' }, // eventek
-      { id:"07",title: 'Eventttt', start: '2021-08-15T14:30:00' ,end:'2021-08-15T17:30:00'}, // eventek
+      { id:"07",title: 'Eventttt', start: '2021-08-16T14:30:00' ,end:'2021-08-16T17:30:00',
+      extendedProps: {eventText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies quam et fringilla convallis mauris. Fermentum tempor nunc, faucibus adipiscing gravida suspendisse. Iaculis in sit a lectus dolor massa pretium ut. Orci blandit nunc ut cum felis arcu. Dictum aliquet quisque imperdiet purus, vitae accumsan posuere amet.'}}, // eventek
       { id:"08",title: 'Event', date: '2021-08-01' }, // eventek
       { id:"09",title: 'Event', date: '2021-08-01' }, // eventek
       { id:"10",title: 'Event', date: '2021-08-01' }, // eventek
